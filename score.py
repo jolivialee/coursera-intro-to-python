b703a1e9c974b207e4e36046207c4bb8cbb06427
Olivia Lee <49142717+jolivialee@users.noreply.github.com>
2019-04-01 03:58:34 -0400

    create score.py

--- score.py
+++ score.py
@@ -1,4 +1,5 @@
-# Prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message. If the score is within range, print a grade according to the following table:
+# Prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message
+# If the score is within range, print a grade according to the following table:
 # >= 0.9 A 
 # >= 0.8 B
 # >= 0.7 C
