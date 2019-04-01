daa157f564bdf861455222b21282023b3f3ca02e
jolivialee <49142717+jolivialee@users.noreply.github.com>
2019-04-01 03:31:50 -0400

    Rewrite compute pay
    Modify to take overtime pay into account

---
+++ rewrite-compute-pay.py
@@ -0,0 +1,16 @@
+#Prompt user to compute pay with overtime pay. Hours: 45; rate: 10; pay: 475.0
+
+hrs = input("Enter Hours: ")
+rte = input ("Enter Rate: ")
+h = float(hrs)
+r = float(rte)
+
+if h > 40:
+    reg = 40*r
+    ot = (h-40)*1.5*r
+    final = reg + ot
+
+else: 
+    final = h*r
+
+print ("pay should be", final)
